5cd0b70c415b941e343a60ecd136e7fddc568024
Shabbeer Petersen <shabbeer.petersen@capaciti.org.za>
2024-12-02 10:23:02 +0200

    learning explicit types

---
+++ hello.js
@@ -0,0 +1,5 @@
+// greet the world
+function greet(person, date) {
+    console.log("Hello ".concat(person, ", today is ").concat(date.toDateString()));
+}
+greet("Maddison", Date());
---
+++ hello.ts
@@ -0,0 +1,6 @@
+// greet the world
+function greet(person: string, date: Date) {
+    console.log(`Hello ${person}, today is ${date.toDateString()}`);
+}
+
+greet("Maddison", Date());
