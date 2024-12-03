cfa8bec43a4204ff4b043fa34c721c0ba9530d5c
Shabbeer Petersen <shabbeer.petersen@capaciti.org.za>
2024-12-03 15:11:04 +0200

    practice

--- hello.js
+++ hello.js
@@ -1,13 +1,33 @@
-var Human = /** @class */ (function () {
-    function Human(data) {
-        this.name = data;
+var variable = "extra";
+var varLength = variable.length;
+// Creating Arrays
+var strings = ["one", "two", "three", "four", "five"];
+var numbers = [1, 2, 3, 4, 5,];
+//Multitype Arrays
+var strAndNum = ['age', 20];
+//multidimensional array
+var numbersArray = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]];
+//Tuples
+var exampleTuple = [20, "twenty"];
+//Enums
+var State;
+(function (State) {
+    State[State["Playing"] = 0] = "Playing";
+    State[State["Paused"] = 1] = "Paused";
+    State[State["Stopped"] = 2] = "Stopped";
+})(State || (State = {}));
+//Objects
+var human = {
+    firstname: 'Frank',
+    age: 32,
+    height: 200,
+    greet: function () {
+        if (human.height >= 200) {
+            return console.log("hello ".concat(human.firstname, ", you long ass motherfucker"));
+        }
+        else {
+            return console.log("hello there ".concat(human.firstname));
+        }
     }
-    return Human;
-}());
-var human = new Human('Gabriel');
-if (human instanceof Human) {
-    console.log("".concat(human.name, " is a human"));
-}
-var str = "extra";
-var strLength = str.length;
-console.log(strLength);
+};
+human.greet();
--- hello.ts
+++ hello.ts
@@ -1,15 +1,40 @@
-class Human {
-    name: string;
-    constructor( data: string) {
-        this.name = data;
+let variable: any = "extra"
+let varLength = (<string>variable).length;
+
+// Creating Arrays
+let strings: string[] = ["one", "two", "three", "four","five"];
+
+let numbers: Array<number> = [1, 2, 3, 4, 5,]
+
+//Multitype Arrays
+let strAndNum: (string | number)[] = ['age', 20]
+
+//multidimensional array
+let numbersArray: number[][] = [[1,2,3,4,5],[6,7,8,9,10]]
+
+//Tuples
+let exampleTuple: [number, string] = [20, "twenty"];
+
+//Enums
+enum State{
+    Playing,
+    Paused,
+    Stopped,
+}
+
+//Objects
+const human = {
+    firstname: 'Frank',
+    age: 32,
+    height: 218,
+    greet: ()=> {
+        if (human.height >= 200) {
+            return console.log(`hello ${human.firstname}, you long ass motherfucker`)
+        }else { return console.log(`hello there ${human.firstname}`)}
     }
-}
+};
+//human.greet();
 
-let human = new Human('Gabriel');
-if (human instanceof Human) {
-    console.log(`${human.name} is a human`);
-}
+type Human = {firstName: string, age: number, height: number};
 
-let str: any = "extra"
-let strLength = (str as string).length;
-console.log(strLength);
+const John: Human = {firstName: "John", age: 20, height: 170};
